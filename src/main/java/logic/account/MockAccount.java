ad178908dcfa8541b464710fbfad97360e8c2dfa
bcy97 <609233559@qq.com>
2016-11-05 11:58:01 +0800

    bcyMock（未完待续）

---
+++ src/main/java/logic/account/MockAccount.java
@@ -0,0 +1,15 @@
+package logic.account;
+
+public class MockAccount {
+	String user_id;
+	String password;
+	String identity;
+	public MockAccount(String user_id,String password,String identity) {
+		this.user_id=user_id;
+		this.password=password;
+	}
+	
+	public String  getIdentity(String user_id) {
+		return identity;
+	}
+}
---
+++ src/main/java/logic/room/MockRoom.java
@@ -0,0 +1,5 @@
+package logic.room;
+
+public class MockRoom {
+	
+}
