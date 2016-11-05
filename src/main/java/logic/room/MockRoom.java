66bf3c5bce3398a158dc8ac88d1eeb00b9c2994e
bcy97 <609233559@qq.com>
2016-11-05 12:24:59 +0800

    bcyMock（完成1.1）

--- src/main/java/logic/room/MockRoom.java
+++ src/main/java/logic/room/MockRoom.java
@@ -6,18 +6,22 @@ import vo.RoomVO;
 public class MockRoom {
 	
 	public ArrayList<RoomVO> getRoomList(String hotel_id) {
+		System.out.println("getRoomList调用成功");
 		return null;
 	}
 	
 	public RoomVO getRoomInfo(String hotel_id,String room_id){
+		System.out.println("getRoomInfo调用成功");
 		return null;
 	}
 	
 	public boolean updateRoomInfo(RoomVO room){
+		System.out.println("updateRoomInfo调用成功");
 		return true;
 	}
 	
 	public boolean addRoomInfo(RoomVO room){
+		System.out.println("addRoomInfo调用成功");
 		return true;
 	}
 }
