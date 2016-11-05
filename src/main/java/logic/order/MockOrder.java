10f4a3208e9450e0f1756aa9fcf6d80d925a155a
IMWYY <Mark.W@wangyouyundeMacBook-Pro.local>
2016-11-05 14:53:11 +0800

    order的mock代码

--- src/main/java/logic/order/MockOrder.java
+++ src/main/java/logic/order/MockOrder.java
@@ -1,8 +1,20 @@
 package logic.order;
 
-public class MockOrder {
-	public MockOrder(){
+import vo.OrderVO;
 
+public class MockOrder {
+	
+	String orderid;
+	
+	public MockOrder(String orderid){
+		this.orderid = orderid;
+	}
+	
+	public OrderVO getOrderInfo(String Id) {
+		
+		System.out.println("getOrderInfo(String userid)方法已经调用");
+		
+		return null;
 	}
 	
 }
