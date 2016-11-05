c72fbd39136987bf9af2972ebe705b679c2085b3
bcy97 <609233559@qq.com>
2016-11-05 12:15:32 +0800

    bcyMock（完成）

--- src/main/java/logic/account/MockAccount.java
+++ src/main/java/logic/account/MockAccount.java
@@ -7,9 +7,11 @@ public class MockAccount {
 	public MockAccount(String user_id,String password,String identity) {
 		this.user_id=user_id;
 		this.password=password;
+		this.identity=identity;
 	}
 	
-	public String  getIdentity(String user_id) {
+	public String getIdentity(String user_id) {
 		return identity;
 	}
+	
 }
--- src/main/java/logic/room/MockRoom.java
+++ src/main/java/logic/room/MockRoom.java
@@ -1,5 +1,23 @@
 package logic.room;
 
+import java.util.ArrayList;
+import vo.RoomVO;
+
 public class MockRoom {
 	
+	public ArrayList<RoomVO> getRoomList(String hotel_id) {
+		return null;
+	}
+	
+	public RoomVO getRoomInfo(String hotel_id,String room_id){
+		return null;
+	}
+	
+	public boolean updateRoomInfo(RoomVO room){
+		return true;
+	}
+	
+	public boolean addRoomInfo(RoomVO room){
+		return true;
+	}
 }
