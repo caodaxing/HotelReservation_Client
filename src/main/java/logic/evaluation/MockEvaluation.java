218c7e4a62aaa875824803598ab2757eca3a4135
DeYuZhao <2239645870@qq.com>
2016-11-05 11:58:46 +0800

    赵德宇删的VO

--- src/main/java/logic/evaluation/MockEvaluation.java
+++ src/main/java/logic/evaluation/MockEvaluation.java
@@ -1,5 +1,13 @@
-package logic.evaluation;
-
-public class MockEvaluation {
-	
-}
+//package logic.evaluation;
+//
+//import vo.EvaluationVO;
+//
+//public class MockEvaluation {
+//	EvaluationVO evaluationInfo;
+//	public MockEvaluation (EvaluationVO eva){
+//		eva = evaluationInfo;
+//	}
+//	public EvaluationVO getEvaluationInfo(){
+//		return evaluationInfo;
+//	}
+//}
---
+++ src/main/java/logic/promotion/MockCalculatePromotion.java
@@ -0,0 +1,15 @@
+package logic.promotion;
+
+import java.util.HashMap;
+
+import logicService.order.Order;
+
+public class MockCalculatePromotion {
+	double price;
+	public MockCalculatePromotion (double p){
+		p = price;
+	}
+	public double calculate(Order order,HashMap<String,String> promotion){
+		return price;
+	}
+}
