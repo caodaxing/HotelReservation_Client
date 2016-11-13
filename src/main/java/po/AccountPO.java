16942c03e7ec62d43c110b8da914533d8789822a
bcy97 <609233559@qq.com>
2016-11-13 18:01:20 +0800

    AccountPO、RoomPO的注释

--- src/main/java/po/AccountPO.java
+++ src/main/java/po/AccountPO.java
@@ -12,6 +12,19 @@ public class AccountPO {
 	String bussinessCall;
 	String phoneNumber;
 	
+	/**
+	 * 
+	 * @param kindOfUser 账户的身份
+	 * @param accountID	帐号
+	 * @param password	密码
+	 * @param isMenber	是否是会员
+	 * @param birthdayOfMember 会员生日
+	 * @param bussinessOfMember 会员公司
+	 * @param trueName 真实姓名
+	 * @param place 账户地址
+	 * @param bussinessCall 公司电话
+	 * @param phoneNumber 电话
+	 */
 	public AccountPO(String kindOfUser,String accountID,String password,boolean isMenber,String birthdayOfMember,
 						String bussinessOfMember,String trueName,String place,String bussinessCall,String phoneNumber){
 		super();
--- src/main/java/po/RoomPO.java
+++ src/main/java/po/RoomPO.java
@@ -10,6 +10,13 @@ public class RoomPO {
 	int numberOfRoom;
 	ArrayList<Image> pictureOfRoom;
 	
+	/**
+	 * @param kindOfRoom 房间种类
+	 * @param priceOfRoom 房间价格
+	 * @param isEmpty 是否空房
+	 * @param numberOfRoom 房间数量
+	 * @param pictureOfRoom 房间图片
+	 */
 	public RoomPO(String kindOfRoom,int priceOfRoom,boolean isEmpty,int numberOfRoom,ArrayList<Image> pictureOfRoom) {
 		super();
 		this.kindOfRoom=kindOfRoom;
