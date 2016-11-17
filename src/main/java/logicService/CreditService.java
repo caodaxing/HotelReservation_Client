862452e4087e25d381bba11e3595c9ab3d1a8b33
siratori <741333496@qq.com>
2016-11-17 20:53:51 +0800

    王雪加接口

--- src/main/java/logicService/CreditService.java
+++ src/main/java/logicService/CreditService.java
@@ -1,5 +1,17 @@
 package logicService;
 
-public interface CreditService {
+import java.util.ArrayList;
+import Message.CreditChange;
+import vo.CreditVO;
 
+public interface CreditService {
+	
+	public boolean changeCredit(String user_ID , CreditChange change , int number );
+	
+	public int getCredit(String user_ID);
+	
+	public ArrayList<CreditVO> getHistoryList(String user_ID);
+	
+	public boolean setVIPCredit(int credit_num);
+	
 }
--- src/main/java/logicService/UserService.java
+++ src/main/java/logicService/UserService.java
@@ -1,5 +1,39 @@
 package logicService;
 
-public interface UserService {
+import java.util.ArrayList;
 
+import Message.Identity;
+import vo.AccountVO;
+import vo.HotelInfoVO;
+import vo.HotelManagerVO;
+import vo.UserVO;
+import vo.VIPInfoVO;
+import vo.WebBusinessVO;
+
+public interface UserService {
+	
+	public UserVO getUserInfo(String user_ID);
+	
+	public boolean updateUserInfo(UserVO new_userInfo , String user_ID);
+	
+	public boolean setVIP(VIPInfoVO VIPInfo , String user_ID);
+	
+	public VIPInfoVO getVIPInfo (String user_ID);
+	
+	public ArrayList<HotelInfoVO> getHistoryHotelInfo(String user_ID);
+	
+	public boolean addHotelManagerInfo(HotelManagerVO hotelManager ,String hotel_ID);
+	
+	public HotelManagerVO getHotelManagerInfo(String hotel_ID);
+	
+	public boolean updateHotelManagerInfo(HotelManagerVO hotelManagerInfo , String hotel_ID);
+	
+	public boolean addWebBusiness(WebBusinessVO  webBusinessInfo);
+	
+	public WebBusinessVO getWebBusinessInfo(String webBusiness_ID);
+	
+	public boolean updateWebBusinessInfo(WebBusinessVO webBusinessInfo , String webBusiness_ID);
+	
+	public Identity getAccountIdentity(AccountVO accountVO);
+	
 }
