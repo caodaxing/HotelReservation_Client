128cfa1f1e1d6a9f546d3135be37c4c9099304fd
DeYuZhao <2239645870@qq.com>
2016-11-24 19:02:40 +0800

    一丢丢界面

--- src/main/java/view/left/UserUI.java
+++ src/main/java/view/left/UserUI.java
@@ -1,14 +1,37 @@
 package view.left;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  * 客户的左侧导航栏
  * @author XueWang
  *
  */
-public class UserUI {
+public class UserUI extends Application{
 	
 	public void refreshLeft(){
 		
 	}
 	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		primaryStage.setTitle("Rookie Travel");
+		primaryStage.show();
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.BASELINE_LEFT);
+		grid.setVgap(10);
+		Scene scene = new Scene(grid, 800, 600);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
