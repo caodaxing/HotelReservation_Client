6e35764667ffa594c4d7b85d9ac1639cec212a59
siratori <741333496@qq.com>
2016-11-24 19:57:59 +0800

    王雪 修改account界面

--- src/main/java/view/account/FirstUI.java
+++ src/main/java/view/account/FirstUI.java
@@ -1,5 +1,10 @@
 package view.account;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * 打开系统的第一个页面
  * @author XueWang
@@ -7,4 +12,23 @@ package view.account;
  */
 public class FirstUI {
 
+	public void start (Stage stage){
+		
+		try{
+			
+			Parent root = FXMLLoader.load( getClass().getResource("首页.fxml") );
+			
+			Scene scene = new Scene(root , 800 , 600);
+			
+			stage.setScene(scene);
+			stage.show();
+			
+		}catch(Exception e){
+			
+			e.printStackTrace();
+		
+		}
+		
+	}
+	
 }
--- src/main/java/view/account/SignUpUI.java
+++ src/main/java/view/account/SignUpUI.java
@@ -1,5 +1,10 @@
 package view.account;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * 注册界面
  * @author XueWang
@@ -7,4 +12,23 @@ package view.account;
  */
 public class SignUpUI {
 
+	public void start (Stage stage){
+		
+		try{
+			
+			Parent root = FXMLLoader.load( getClass().getResource("登录界面.fxml") );
+			
+			Scene scene = new Scene(root , 800 , 600);
+			
+			stage.setScene(scene);
+			stage.show();
+			
+		}catch(Exception e){
+			
+			e.printStackTrace();
+		
+		}
+		
+	}
+	
 }
