99ede6c6cafda5f0076287387dc811da8cf2c822
IMWYY <Mark.W@wangyouyundeMacBook-Pro.local>
2016-12-03 16:30:20 +0800

    遗漏的合并。logicservice的stub由ui层根据需要来写。。

--- src/main/java/logicService/stub/OrderService_Stub.java
+++ src/main/java/logicService/stub/OrderService_Stub.java
@@ -62,4 +62,32 @@ public class OrderService_Stub implements OrderService {
 	}
 
 	
+public ArrayList<OrderVO> getDailyOrderList() {
+		
+//		ArrayList<OrderVO> List = new ArrayList<OrderVO>();
+//		OrderVO order1 = new OrderVO("161124000021109", "161124", new RoomVO(0, 288, 0, "1109"), "161125", 0, 2, false, 288);
+//		OrderVO order2 = new OrderVO("161225005540304", "161225", new RoomVO(0, 388, 0, "0304"), "161226", 1, 2, true, 388);
+//		List.add(order1);
+//		List.add(order2);
+//		
+//		return List;
+		return null;
+	}
+
+	public ResultMessage undoUnnormalOrder(OrderVO orderVO) {
+		return ResultMessage.SUCCESS;
+	}
+
+	public ResultMessage normalExecute(String order_id) {
+		return ResultMessage.FAILURE;
+	}
+
+	public ResultMessage autoSetUnnormal(String order_id) {
+		return ResultMessage.FAILURE;
+	}
+
+	public ResultMessage supplyOrder(String order_id) {
+		return ResultMessage.FAILURE;
+	}
+	
 }
