59bb219d6bf55a794647934880af46dff5a54098
bcy97 <609233559@qq.com>
2016-12-07 23:23:37 +0800

    王歪歪注意一下
    我修改了RoomInfo的接口，增加了一个getRoomList的方法

--- src/main/java/logic/mockObject/MockGetRoomInfo.java
+++ src/main/java/logic/mockObject/MockGetRoomInfo.java
@@ -1,5 +1,7 @@
 package logic.mockObject;
 
+import java.util.ArrayList;
+
 import Message.RoomType;
 import logic.room.GetRoomInfo;
 import vo.RoomVO;
@@ -9,6 +11,11 @@ public class MockGetRoomInfo implements GetRoomInfo{
 	@Override
 	public RoomVO getRoomInfo(String hotel_id, String room_id) {
 		return new RoomVO("green", "1109", RoomType.STANDARD_ROOM, 300, false, null);
+	}
+
+	@Override
+	public ArrayList<RoomVO> getRoomList(String hotelID) {
+		return null;
 	}
 
 }
