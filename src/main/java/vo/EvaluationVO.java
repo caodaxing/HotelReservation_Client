9be15c506c405d3bf697b0227f40a493496551c8
bcy97 <609233559@qq.com>
2016-12-10 11:17:35 +0800

    修改EvaluationVO

--- src/main/java/vo/EvaluationVO.java
+++ src/main/java/vo/EvaluationVO.java
@@ -4,16 +4,7 @@ import Message.RoomType;
 
 /**
  * @param orderID 订单id
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @param commentLevel 评分 （保留一位小数）
-=======
-=======
->>>>>>> origin/master
- * @param orderId 房间类型 
- * @param checkInTime 入住时间
- * @param commentLevel 评分
->>>>>>> origin/master
  * @param evaluationContent 评价内容
  */
 public class EvaluationVO {
