9b4c66525e894bbe5838785b0f44b58e55941a9b
IMWYY <Mark.W@wangyouyundeMacBook-Pro.local>
2016-12-10 22:16:04 +0800

    stub的一点点修改

--- src/main/java/dataDao/stub/AccountDao_Stub.java
+++ src/main/java/dataDao/stub/AccountDao_Stub.java
@@ -1,10 +1,21 @@
 package dataDao.stub;
 
+import java.util.ArrayList;
+
+import Message.Identity;
 import dataDao.account.AccountDao;
 import po.AccountPO;
 
 
 public class AccountDao_Stub implements AccountDao{
+	
+	private ArrayList<AccountPO> pos = new ArrayList<AccountPO>();
+
+	public AccountDao_Stub() {
+		this.pos.add(new AccountPO("wyy", "e80b5017098950fc58aad83c8c14978e", Identity.CLIENT.ordinal()));
+		this.pos.add(new AccountPO("bcy", "162befe78a0c20249a4bab30cca88515", Identity.CLIENT.ordinal()));
+		this.pos.add(new AccountPO("zdy", "e80b5017098950fc58aad83c8c14978e", Identity.CLIENT.ordinal()));
+	}
 
 	public boolean addAccount(AccountPO po) {
 		return true;
