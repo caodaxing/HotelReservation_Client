777ffc38733dd4ccfeb70c7cefea2b107bf7eeeb
IMWYY <Mark.W@wangyouyundeMacBook-Pro.local>
2016-12-13 13:05:04 +0800

    更改一下vo

--- src/main/java/vo/EvaluationVO.java
+++ src/main/java/vo/EvaluationVO.java
@@ -9,7 +9,7 @@ public class EvaluationVO {
 	
 	public String orderID;
 	public double commentLevel;
-	public String evaluationContent;
+	public String evaluationContent = "";
 	
 	public EvaluationVO(String orderID, double commentLevel, String evaluationContent) {
 		this.orderID = orderID;
