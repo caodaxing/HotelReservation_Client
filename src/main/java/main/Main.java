91a901e2abc682b74eacf296f3465b42ec8395ba
13894858312 <741333496@qq.com>
2016-12-20 18:55:16 +0800

    删除main中无用的

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,10 @@
 package main;
 
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.rmi.RemoteException;
-
-import dataDao.account.AccountDao;
-import dataDao.picture.PictureDao;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import main.rmi.ClientRunner;
-import main.rmi.RemoteHelper;
 import viewController.AccountController;
 
 public class Main extends Application{
@@ -39,16 +30,5 @@ public class Main extends Application{
 		
 		launch(args);
 		
-//		AccountDao ad = RemoteHelper.getInstance().getAccountDao();
-//		
-//		PictureDao pictureDao = RemoteHelper.getInstance().getPictureDao();
-//		
-//		String imgFile = "d:\\PictureDatabase\\00001\\1.jpg";  
-//        
-//		try {
-//			System.out.println(pictureDao.saveImage(PictureHelper.imageToBytes(imgFile), "12345", "1"));
-//		} catch (RemoteException e) {
-//			e.printStackTrace();
-//		}
 	}
 }
